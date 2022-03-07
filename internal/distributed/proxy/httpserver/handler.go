e58ae27daa96836f2610f709bdd703e6326f5301
shaoyue.chen <shaoyue.chen@zilliz.com>
2022-03-07 23:04:34 +0800

    implements restful entities api
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- internal/distributed/proxy/httpserver/handler.go
+++ internal/distributed/proxy/httpserver/handler.go
@@ -24,6 +24,13 @@ func NewHandlers(proxy types.ProxyComponent) *Handlers {
 func (h *Handlers) RegisterRoutesTo(router gin.IRouter) {
 	router.GET("/health", wrapHandler(h.handleGetHealth))
 	router.POST("/dummy", wrapHandler(h.handlePostDummy))
+
+	router.POST("/entities", wrapHandler(h.handleInsert))
+	router.DELETE("/entities", wrapHandler(h.handleDelete))
+	router.GET("/entities", wrapHandler(h.handleSearchAndQuery))
+
+	router.POST("/persist", wrapHandler(h.handleFlush))
+	router.GET("/distance", wrapHandler(h.handleCalcDistance))
 }
 
 func (h *Handlers) handleGetHealth(c *gin.Context) (interface{}, error) {
@@ -33,9 +40,71 @@ func (h *Handlers) handleGetHealth(c *gin.Context) (interface{}, error) {
 func (h *Handlers) handlePostDummy(c *gin.Context) (interface{}, error) {
 	req := milvuspb.DummyRequest{}
 	// use ShouldBind to supports binding JSON, XML, YAML, and protobuf.
-	err := c.ShouldBind(&req)
+	err := shouldBind(c, &req)
 	if err != nil {
-		return nil, fmt.Errorf("%w: parse json failed: %v", errBadRequest, err)
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
 	}
 	return h.proxy.Dummy(c, &req)
+}
+
+func (h *Handlers) handleInsert(c *gin.Context) (interface{}, error) {
+	req := milvuspb.InsertRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Insert(c, &req)
+}
+
+func (h *Handlers) handleDelete(c *gin.Context) (interface{}, error) {
+	req := milvuspb.DeleteRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Delete(c, &req)
+}
+
+func (h *Handlers) handleSearchAndQuery(c *gin.Context) (interface{}, error) {
+	_, byQuery := c.GetQuery("by_query")
+	if byQuery {
+		return h.handleQuery(c)
+	}
+	return h.handleSearch(c)
+}
+
+func (h *Handlers) handleSearch(c *gin.Context) (interface{}, error) {
+	req := milvuspb.SearchRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Search(c, &req)
+}
+
+func (h *Handlers) handleQuery(c *gin.Context) (interface{}, error) {
+	req := milvuspb.QueryRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Query(c, &req)
+}
+
+func (h *Handlers) handleFlush(c *gin.Context) (interface{}, error) {
+	req := milvuspb.FlushRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.Flush(c, &req)
+}
+
+func (h *Handlers) handleCalcDistance(c *gin.Context) (interface{}, error) {
+	req := milvuspb.CalcDistanceRequest{}
+	err := shouldBind(c, &req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: parse body failed: %v", errBadRequest, err)
+	}
+	return h.proxy.CalcDistance(c, &req)
 }
--- internal/distributed/proxy/httpserver/handler_test.go
+++ internal/distributed/proxy/httpserver/handler_test.go
@@ -3,13 +3,17 @@ package httpserver
 import (
 	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/milvus-io/milvus/internal/proto/commonpb"
 	"github.com/milvus-io/milvus/internal/proto/milvuspb"
+	"github.com/milvus-io/milvus/internal/proto/schemapb"
 	"github.com/milvus-io/milvus/internal/types"
 	"github.com/stretchr/testify/assert"
 )
@@ -25,8 +29,56 @@ func (mockProxyComponent) Dummy(ctx context.Context, request *milvuspb.DummyRequ
 	return nil, nil
 }
 
+func (mockProxyComponent) Insert(ctx context.Context, request *milvuspb.InsertRequest) (*milvuspb.MutationResult, error) {
+	if request.CollectionName == "" {
+		return nil, errors.New("body parse err")
+	}
+	return &milvuspb.MutationResult{Acknowledged: true}, nil
+}
+
+func (mockProxyComponent) Delete(ctx context.Context, request *milvuspb.DeleteRequest) (*milvuspb.MutationResult, error) {
+	if request.Expr == "" {
+		return nil, errors.New("body parse err")
+	}
+	return &milvuspb.MutationResult{Acknowledged: true}, nil
+}
+
+func (mockProxyComponent) Search(ctx context.Context, request *milvuspb.SearchRequest) (*milvuspb.SearchResults, error) {
+	if request.Dsl == "" {
+		return nil, errors.New("body parse err")
+	}
+	return &milvuspb.SearchResults{
+		Results: &schemapb.SearchResultData{
+			TopK: 10,
+		},
+	}, nil
+}
+
+func (mockProxyComponent) Query(ctx context.Context, request *milvuspb.QueryRequest) (*milvuspb.QueryResults, error) {
+	if request.Expr == "" {
+		return nil, errors.New("body parse err")
+	}
+	return &milvuspb.QueryResults{
+		CollectionName: "test",
+	}, nil
+}
+
+func (mockProxyComponent) Flush(ctx context.Context, request *milvuspb.FlushRequest) (*milvuspb.FlushResponse, error) {
+	if len(request.CollectionNames) < 1 {
+		return nil, errors.New("body parse err")
+	}
+	return &milvuspb.FlushResponse{}, nil
+}
+
+func (mockProxyComponent) CalcDistance(ctx context.Context, request *milvuspb.CalcDistanceRequest) (*milvuspb.CalcDistanceResults, error) {
+	if len(request.Params) < 1 {
+		return nil, errors.New("body parse err")
+	}
+	return &milvuspb.CalcDistanceResults{}, nil
+}
+
 func TestHandlers(t *testing.T) {
-	mockProxy := mockProxyComponent{}
+	mockProxy := &mockProxyComponent{}
 	h := NewHandlers(mockProxy)
 	testEngine := gin.New()
 	h.RegisterRoutesTo(testEngine)
@@ -85,5 +137,113 @@ func TestHandlers(t *testing.T) {
 		w := httptest.NewRecorder()
 		testEngine.ServeHTTP(w, req)
 		assert.Equal(t, http.StatusOK, w.Code)
+	})
+
+	t.Run("handleFlushRequest ok", func(t *testing.T) {
+		bodyStruct := milvuspb.FlushRequest{CollectionNames: []string{"c1"}}
+		body, _ := json.Marshal(bodyStruct)
+		req := httptest.NewRequest(http.MethodPost, "/persist", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, []byte(`{}`), w.Body.Bytes())
+	})
+	t.Run("handleFlushRequest bad req", func(t *testing.T) {
+		body := []byte("bad request")
+		req := httptest.NewRequest(http.MethodPost, "/persist", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+
+	t.Run("handleCalculateDistance ok", func(t *testing.T) {
+		bodyStruct := milvuspb.CalcDistanceRequest{
+			Params: []*commonpb.KeyValuePair{
+				{Key: "key", Value: "val"},
+			}}
+		body, _ := json.Marshal(bodyStruct)
+		req := httptest.NewRequest(http.MethodGet, "/distance", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+	})
+	t.Run("handleCalculateDistance bad req", func(t *testing.T) {
+		body := []byte("bad request")
+		req := httptest.NewRequest(http.MethodGet, "/distance", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+}
+
+func TestEntitiesHandlers(t *testing.T) {
+	mockProxy := &mockProxyComponent{}
+	h := NewHandlers(mockProxy)
+	testEngine := gin.New()
+	h.RegisterRoutesTo(testEngine)
+
+	t.Run("handleInsertRequest ok", func(t *testing.T) {
+		bodyStruct := milvuspb.InsertRequest{CollectionName: "c1"}
+		body, _ := json.Marshal(bodyStruct)
+		req := httptest.NewRequest(http.MethodPost, "/entities", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, []byte(`{"acknowledged":true}`), w.Body.Bytes())
+	})
+	t.Run("handleInsertRequest bad req", func(t *testing.T) {
+		body := []byte("bad request")
+		req := httptest.NewRequest(http.MethodPost, "/entities", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+	t.Run("handleDeleteRequest ok", func(t *testing.T) {
+		bodyStruct := milvuspb.DeleteRequest{Expr: "some expr"}
+		body, _ := json.Marshal(bodyStruct)
+		req := httptest.NewRequest(http.MethodDelete, "/entities", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, []byte(`{"acknowledged":true}`), w.Body.Bytes())
+	})
+	t.Run("handleDeleteRequest bad req", func(t *testing.T) {
+		body := []byte("bad request")
+		req := httptest.NewRequest(http.MethodDelete, "/entities", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+	t.Run("handleSearchRequest ok", func(t *testing.T) {
+		bodyStruct := milvuspb.SearchRequest{Dsl: "some dsl"}
+		body, _ := json.Marshal(bodyStruct)
+		req := httptest.NewRequest(http.MethodGet, "/entities", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, []byte(`{"results":{"top_k":10}}`), w.Body.Bytes())
+	})
+	t.Run("handleSearchRequest bad req", func(t *testing.T) {
+		body := []byte("bad request")
+		req := httptest.NewRequest(http.MethodGet, "/entities", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+	})
+	t.Run("handleQueryRequest ok", func(t *testing.T) {
+		bodyStruct := milvuspb.QueryRequest{Expr: "some expr"}
+		body, _ := json.Marshal(bodyStruct)
+		req := httptest.NewRequest(http.MethodGet, "/entities?by_query", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.Equal(t, []byte(`{"collection_name":"test"}`), w.Body.Bytes())
+	})
+	t.Run("handleQueryRequest bad req", func(t *testing.T) {
+		body := []byte("bad request")
+		req := httptest.NewRequest(http.MethodGet, "/entities?by_query", bytes.NewReader(body))
+		w := httptest.NewRecorder()
+		testEngine.ServeHTTP(w, req)
+		assert.Equal(t, http.StatusBadRequest, w.Code)
 	})
 }
