b0043f07dc2d46d2b97b124ae4fa0b30f2f5ecbd
haorenfsa <haorenfsa@gmail.com>
2024-07-20 11:13:04 +0800

    Gin & restfulv1 handler use sonic json
    Signed-off-by: haorenfsa <haorenfsa@gmail.com>

--- internal/distributed/proxy/httpserver/utils.go
+++ internal/distributed/proxy/httpserver/utils.go
@@ -3,7 +3,6 @@ package httpserver
 import (
 	"bytes"
 	"encoding/binary"
-	"encoding/json"
 	"fmt"
 	"math"
 	"reflect"
@@ -20,6 +19,7 @@ import (
 	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/milvuspb"
 	"github.com/milvus-io/milvus-proto/go-api/v2/schemapb"
+	"github.com/milvus-io/milvus/internal/json"
 	"github.com/milvus-io/milvus/pkg/common"
 	"github.com/milvus-io/milvus/pkg/log"
 	"github.com/milvus-io/milvus/pkg/util"
